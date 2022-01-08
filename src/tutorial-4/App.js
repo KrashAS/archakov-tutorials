3e443958ab1fb35e22748276eebb9d8dd85e462a
KrashAS <askrashennikov@gmail.com>
2022-01-08 15:33:01 +0200

    Внёс правки по замечаниям

--- src/tutorial-4/App.js
+++ src/tutorial-4/App.js
@@ -1,100 +1,44 @@
 import React from "react";
 import Phrase from "./components/Phrase";
 import EmptyBlock from "./components/EmptyBlock";
+import adjectivesArr from "./data/DataAdjectivesArr.js";
+import nounsArr from "./data/DataNounsArr.js";
 
-function RenderRandomPhrase() {
-  const adjectivesArr = [
-    "абсолютный",
-    "адский",
-    "азартный",
-    "активный",
-    "ангельский",
-    "астрономический",
-    "баснословный",
-    "безбожный",
-    "безбрежный",
-    "безвозвратный",
-    "безграничный",
-    "бездонный",
-    "бездушный",
-    "безжалостный",
-    "замечательно",
-    "замечательный",
-    "записной",
-    "запредельный",
-    "заядлый",
-    "звериный",
-    "зверский",
-    "зеленый",
-    "злой",
-    "злостный",
-    "значительный",
-    "неоспоримый",
-    "неотразимый",
-    "неоценимый",
-    "непередаваемый",
-  ];
-  const nounsArr = [
-    "лгун",
-    "день",
-    "конь",
-    "олень",
-    "человек",
-    "программист",
-    "ребёнок",
-    "конец",
-    "город",
-    "дурак",
-  ];
-  const [phrase, setPhrase] = React.useState([]);
-  const [render, setRender] = React.useState("");
-
+export default function App() {
+  const [phrases, setPhrase] = React.useState([]);
   const generateRandomWord = (arr) => {
-    const copy = [...arr];
-    const output = [];
-    if (copy.length > 0) {
-      output.push(copy.splice(Math.floor(Math.random() * copy.length), 1));
-    }
-    return output;
+    return arr[Math.floor(Math.random() * arr.length)];
   };
-
-  function generatePhrase() {
+  function renderPhrases() {
     let firstAdjective = generateRandomWord(adjectivesArr);
     let secondAdjective = generateRandomWord(adjectivesArr);
     let randomNouns = generateRandomWord(nounsArr);
-
-    phrase.push(`${firstAdjective} ${secondAdjective} ${randomNouns}`);
-
-    setPhrase(phrase);
-    setRender(
-      phrase.map((value, index, arr) => (
-        <Phrase key={index} text={phrase[index].toString()} />
-      ))
-    );
+    const randomPhrase = `${firstAdjective} ${secondAdjective} ${randomNouns}`;
+    setPhrase([randomPhrase, ...phrases]);
   }
-
   function clearPhrase() {
-    setRender(<EmptyBlock />);
     setPhrase([]);
   }
 
   return (
-    <div className="wrapper">
-      <div className="list">{render}</div>
-      <button className="btn btn_generate" onClick={generatePhrase}>
-        Сгенерировать
-      </button>
-      <button className="btn btn_clear" onClick={clearPhrase}>
-        Очистить
-      </button>
-    </div>
-  );
-}
-
-export default function App() {
-  return (
     <div id="app">
-      <RenderRandomPhrase />
+      <div className="wrapper">
+        <div className="list">
+          {phrases.length > 0 ? (
+            phrases.map((value, index, arr) => (
+              <Phrase key={index} text={phrases[index]} />
+            ))
+          ) : (
+            <EmptyBlock />
+          )}
+        </div>
+        <button className="btn btn_generate" onClick={renderPhrases}>
+          Сгенерировать
+        </button>
+        <button className="btn btn_clear" onClick={clearPhrase}>
+          Очистить
+        </button>
+      </div>
     </div>
   );
 }
