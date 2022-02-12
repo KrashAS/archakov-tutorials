8fed9aab271c2706ea023a758b44a93d59ad6a7d
KrashAS <askrashennikov@gmail.com>
2022-02-12 12:58:20 +0200

    Задание №9 выполнил

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./tutorial-7/App.js";
-import "./tutorial-7/style.css";
+import App from "./tutorial-8/App.js";
+//import "./tutorial-7/style.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
